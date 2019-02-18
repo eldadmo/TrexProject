26cbc29e6ac992194a4e4076a43f1ce18715b7ed
Eldad Michaeli <eldadm@codevalue.net>
2019-02-18 16:15:02 +0200

    Add ability to write data into database

---
+++ Trex2/Trex2.Services/Data/DetailsContext.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Internal;
+using Microsoft.Extensions.DependencyInjection;
+using Trex2.Common.Models;
+using Trex2.Services.Contracts;
+
+namespace Trex2.Services.Data
+{
+    public class DetailsContext : DbContext
+    {
+        public DbSet<Person> PersonDetails { get; set; }
+        public DetailsContext(DbContextOptions<DetailsContext> options) : base(options)
+        {
+        }
+
+        public DetailsContext()
+        {
+            SeedData();
+            //By default it will not track
+            //ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=PersonDetails;Trusted_Connection=True;");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Person>().HasKey(x => x.Id);
+        }
+
+        private void SeedData()
+        {
+            Database.EnsureCreated();
+            if (PersonDetails.Any()) return;
+            PersonDetails.AddRange(new List<Person>()
+                {
+                    new Person(){FirstName = "Eldad", LastName = "Micaheli", Email = ""}
+                });
+            SaveChanges();
+        }
+    }
+}
