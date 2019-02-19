cff698fbb54a665ad7048dfe147317ffa1b3b9ea
Eldad Michaeli <eldadmo@gmail.com>
2019-02-19 15:41:52 +0200

    Features/local db (#5)
    * Add ability to write data into database
    
    * Add local database and configuration option

---
+++ Trex2/Trex2.Services/Contracts/IDetailsController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Trex2.Common.Models;
+
+namespace Trex2.Services.Contracts
+{
+    public interface IDetailsController
+    {
+        Task<List<Person>> Get();
+        Task<Person> Get(int PersonId);
+        Task<Person> Post(Person Person);
+        Task<Person> Put(int PersonId, Person Person);
+        Task<bool> Delete(int PersonId);
+    }
+}
